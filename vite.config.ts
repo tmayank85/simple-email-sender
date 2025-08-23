977789e2ddef6f5d07c69d78f03d490a75eaeba3
gamantyagi <tmayank85@gmail.com>
2025-08-23 15:52:38 +0530

    backend url added

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://simple-email-sender-mediator.vercel.app/',
         changeOrigin: true,
         secure: false,
       },
